13f4d5550ce964e876576f90e897d1847ae52574
oscartroncoso <oscarrd8@gmail.com>
2023-10-30 11:58:49 -0700

    Story updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-import Square from "./components/Square"
+import Square from "./components/Square";
 
-import "./App.css"
+import "./App.css";
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -14,34 +14,51 @@ const App = () => {
     "?",
     "?",
     "?",
-    "?"
-  ])
+    "?",
+  ]);
+
+  const [treasureLocation, setTreasureLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
+  const [bombLocation, setvBombLocation] = useState(
+    Math.floor(Math.random() * board.length)
+  );
 
   const handleSquareClick = (clickedSquareIndex) => {
     //variable holding copy of current state
-    let updatedBoard = [...board]
-   // use index to update the current Square's value with emoji
-   updatedBoard[clickedSquareIndex] = "🌲"
-   setBoard(updatedBoard)
-  }
+    let updatedBoard = [...board];
+    // set condition for if treasure location is same as clicked squares index show a treasure
+    if (clickedSquareIndex === treasureLocation) {
+      // then reassign state value at that index to treasure emoji
+      updatedBoard[clickedSquareIndex] = "💎";
+      // setBoard(updatedBoard)
+    } else if (clickedSquareIndex === bombLocation) {
+      updatedBoard[clickedSquareIndex] = "💣";
+      // setBoard(updatedBoard)
+    } else {
+      // use index from clickedSquareIndex to update the clicked Square value with emoji using the bracket notion
+      updatedBoard[clickedSquareIndex] = "🌲";
+      // update state with new board
+    }
+    setBoard(updatedBoard);
+  };
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
-      <div className = "board">
-      {board.map((value, index) => {
-     return (
-     <Square 
-     value={value} 
-     index ={index} 
-     handleSquareClick={handleSquareClick} 
-     />
-     )
-    })}
-
-   </div>
+      <div className="board">
+        {board.map((value, index) => {
+          return (
+            <Square
+              value={value}
+              index={index}
+              handleSquareClick={handleSquareClick}
+            />
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
